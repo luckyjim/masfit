2948eac3f0f303d6c384238357f16be08c78ec47
luckyjim <jcolley@lpnhe.in2p3.fr>
2024-07-11 09:11:49 +0200

    start

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # masfit
+
+Many and small fit to solve.
+
+* Many over 1.000 identical model to fit
+* Small model less 10 parameters to fit
+
+Goal : do this quickly with GPU 
+
+Method : Sparse matrix like CPU ? JAX ?
---
+++ src/masfit/simu/sampling_model.py
@@ -0,0 +1,17 @@
+'''
+Created on 11 juil. 2024
+
+@author: jcolley
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
